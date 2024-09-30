d4a99886dcc8661a5216ccde973b8019d885f432
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 19:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const form=document.getElementById('form1');
+	const formData=new FormData(form);
+	const firstName=formData.get('fname');
+	const lastName=formData.get('lname');
+	alert(firstName+" "+lastName);
+	
 
 }
